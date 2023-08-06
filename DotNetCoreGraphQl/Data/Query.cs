c80cf80fe00ec0fd66420dba9c96ff65a5d5674b
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-08-06 15:01:42 +0330

    add new queryies

--- DotNetCoreGraphQl/Data/Query.cs
+++ DotNetCoreGraphQl/Data/Query.cs
@@ -11,5 +11,19 @@ namespace DotNetCoreGraphQl.Data
         [UseSorting]
         public IQueryable<Superhero> GetSuperheroes([Service] ApplicationDbContext context) =>
             context.Superheroes;
+
+
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Movie> GetMovies([Service] ApplicationDbContext context) =>
+           context.Movies;
+
+
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Superpower> GetSuperpowers([Service] ApplicationDbContext context) =>
+           context.Superpowers;
     }
 }
